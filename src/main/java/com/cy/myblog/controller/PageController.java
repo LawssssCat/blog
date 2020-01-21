fd92aceff1929fe5511a20e73cc7bb378ecde559
LawssssCat <1191693505@qq.com>
2020-01-21 15:14:27 +0800

    整合-页面分三部分-管理

--- src/main/java/com/cy/myblog/controller/PageController.java
+++ src/main/java/com/cy/myblog/controller/PageController.java
@@ -1,15 +1,27 @@
 package com.cy.myblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 public class PageController {
 	
 	@RequestMapping("/doIndexUI")
-	public String toPage() {
+	public String toIndexUI() {
 		return "/starter"; 
 	}
+	
+	@RequestMapping("/{model}/{page}")
+	public String loadPage(
+			@PathVariable("model") String model , 
+			@PathVariable("page") String page) {
+		String msg = "/"+model+"/"+page ;  
+		log.debug(msg);
+		return msg; 
+	}
+	
 }
