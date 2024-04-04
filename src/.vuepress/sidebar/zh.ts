58c60ce255dd59144f2471b5cc68e7adc20a3778
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2024-04-04 18:32:26 +0800

    init: 删除多余sidebar

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -10,17 +10,6 @@ export const zhSidebar = sidebar({
       link: "demo/",
       children: "structure",
     },
-    {
-      text: "文章",
-      icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
     "intro",
-    {
-      text: "幻灯片",
-      icon: "person-chalkboard",
-      link: "https://plugin-md-enhance.vuejs.press/zh/guide/content/revealjs/demo.html",
-    },
   ],
 });
--- src/zh/router.md
+++ src/zh/router.md
@@ -2,6 +2,7 @@
 title: 目录
 icon: book
 sidebar: false
+article: false
 ---
 
 <Catalog />
