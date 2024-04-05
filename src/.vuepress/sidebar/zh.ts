932d3b3c811ae0aea2b9e2fcc4969e77314a8b0d
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2024-04-05 23:33:16 +0800

    init: 删除 demo

--- src/.vuepress/sidebar/zh.ts
+++ src/.vuepress/sidebar/zh.ts
@@ -1,16 +1,5 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export const zhSidebar = sidebar({
-  "/zh/": [
-    "",
-    {
-      text: "如何使用",
-      icon: "laptop-code",
-      prefix: "demo/",
-      link: "demo/",
-      children: "structure",
-    },
-    "intro",
-  ],
   "/zh/dev-rpc-thrift/": "structure",
 });
