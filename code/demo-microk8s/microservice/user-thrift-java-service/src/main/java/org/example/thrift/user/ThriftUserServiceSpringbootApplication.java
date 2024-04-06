98dfb2191e2090497b29294ff6c860c347853c78
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2024-04-07 07:29:14 +0800

    demo-microk8s: 优化服务器启动方式

--- code/demo-microk8s/microservice/user-thrift-java-service/src/main/java/org/example/thrift/user/ThriftUserServiceSpringbootApplication.java
+++ code/demo-microk8s/microservice/user-thrift-java-service/src/main/java/org/example/thrift/user/ThriftUserServiceSpringbootApplication.java
@@ -1,11 +1,22 @@
 package org.example.thrift.user;
 
+import org.example.thrift.user.thrift.ThriftServer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ThriftUserServiceSpringbootApplication {
+public class ThriftUserServiceSpringbootApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(ThriftUserServiceSpringbootApplication.class, args);
+    }
+
+    @Autowired
+    private ThriftServer thriftServer;
+
+    @Override
+    public void run(String... args) throws Exception {
+        thriftServer.startThriftServer();
     }
 }
--- code/demo-microk8s/microservice/user-thrift-java-service/src/main/java/org/example/thrift/user/thrift/ThriftServer.java
+++ code/demo-microk8s/microservice/user-thrift-java-service/src/main/java/org/example/thrift/user/thrift/ThriftServer.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 
-import javax.annotation.PostConstruct;
-
 @Slf4j
 @Configuration
 public class ThriftServer {
@@ -22,7 +20,6 @@ public class ThriftServer {
     private int thriftServerPort;
     @Autowired
     private UserServiceImpl userServiceImpl;
-    @PostConstruct
     public void startThriftServer() {
         UserService.Processor<UserServiceImpl> userServiceProcessor = new UserService.Processor<>(userServiceImpl);
         try (TServerSocket socket = new TServerSocket(thriftServerPort)){
