ee5330710c4f4cfdb7617a740e14671231c21c3f
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2024-04-16 06:26:59 +0800

    xml(java): 修补测试用例

--- code/demo-java-xml/n12-xstream-usage/src/test/java/example/XmlXStreamTest.java
+++ code/demo-java-xml/n12-xstream-usage/src/test/java/example/XmlXStreamTest.java
@@ -1,7 +1,7 @@
 package example;
 
 import com.thoughtworks.xstream.XStream;
-import com.thoughtworks.xstream.security.NoTypePermission;
+import com.thoughtworks.xstream.io.xml.CompactWriter;
 import lombok.extern.slf4j.Slf4j;
 import org.example.Person;
 import org.junit.jupiter.api.Assertions;
@@ -10,8 +10,10 @@ import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Objects;
 
 @Slf4j
@@ -37,8 +39,9 @@ public class XmlXStreamTest {
         // toXml
         String xml = xStream.toXML(person);
         log.info(xml);
-        try (FileOutputStream fileOutputStream = new FileOutputStream(FILE)) {
-            xStream.toXML(person, fileOutputStream);
+        try (OutputStreamWriter fileOutputStream = new OutputStreamWriter(Files.newOutputStream(FILE.toPath()), StandardCharsets.UTF_8)) {
+            xStream.marshal(person, new CompactWriter(fileOutputStream));
+            // xStream.toXML(person, fileOutputStream);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
