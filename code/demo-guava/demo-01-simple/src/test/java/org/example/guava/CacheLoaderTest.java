c562ac7f9a7bf9df74ea21d69e61bdcfeb68477a
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2024-06-16 08:44:25 +0800

    java(guava): cache

--- code/demo-guava/demo-01-simple/src/test/java/org/example/guava/CacheLoaderTest.java
+++ code/demo-guava/demo-01-simple/src/test/java/org/example/guava/CacheLoaderTest.java
@@ -5,71 +5,126 @@ import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.Lists;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class CacheLoaderTest {
+    /**
+     * 自动缓存加载器基本功能测试
+     * <br>
+     * expire ... 过期策略<br>
+     * reference ... 内存策略<br>
+     * remove listen ... 过期监听<br>
+     * load one / load all ... <br>
+     */
+    @DisplayName("LocalLoadingCache 基本功能测试，如：过期策略、内存策略、过期监听等")
     @Test
-    void testCacheSimple() {
+    void testLocalLoadingCache() {
         LoadingCache<String, String> cache = CacheBuilder.newBuilder()
+                // 基于 ConcurrentHashMap 实现，所以可以设置该类初始化参数
+                .initialCapacity(2) // 初始容量
+                .concurrencyLevel(8) // 并发级别 —— 通过配置 concurrencyLevel，来控制 segment 的数量，来提高并发。ConcurrentHashMap 的默认值是 16，最大值是 1<<16；这里的默认值是 4，最大值为 1<<16。
+                // 过期（非互斥，可同时设置多个）
                 .maximumSize(10) // 最大缓存数量
-                // 过期
+                // .maximumWeight(60) // 最大缓存权重和
+                // .weigher(...) // 权重计算器，设置 maximumWeight 后必须设置 weigher
                 // .expireAfterAccess(30, TimeUnit.SECONDS) // 缓存过期时间
                 // .expireAfterWrite(30, TimeUnit.SECONDS) // 缓存写入后过期时间
                 // .refreshAfterWrite(30, TimeUnit.SECONDS) // 缓存写入后刷新
-                // 引用
-                // .softValues() // 软引用（full gc）
-                // .weakValues() // 弱引用（major gc + full gc）
+                // 过期监听
                 .removalListener(new RemovalListener<String, String>() {
                     @Override
                     public void onRemoval(RemovalNotification<String, String> notification) {
-                        log.info("-----------> remove key: {}", notification.getKey());
+                        String cause = "";
+                        if (RemovalCause.EXPLICIT.equals(notification.getCause())) {
+                            cause = "被显式移除";
+                        } else if (RemovalCause.REPLACED.equals(notification.getCause())) {
+                            cause = "被替换";
+                        } else if (RemovalCause.EXPIRED.equals(notification.getCause())) {
+                            cause = "被过期移除";
+                        } else if (RemovalCause.SIZE.equals(notification.getCause())) {
+                            cause = "被缓存条数超上限移除";
+                        } else if (RemovalCause.COLLECTED.equals(notification.getCause())) {
+                            cause = "被垃圾回收移除";
+                        }
+                        log.debug("-----------> remove key \"{}\" cause \"{}({})\"", notification.getKey(), cause, notification.getCause());
                     }
                 })
-                .build(new CacheLoader<String, String>() {
+                // 引用
+                // .softValues() // 软引用（full gc）
+                // .weakValues() // 弱引用（major gc + full gc）
+                // 统计（hitCount=6, missCount=3, loadSuccessCount=3, loadExceptionCount=0, totalLoadTime=3208400, evictionCount=10）
+                .recordStats() // 开启统计信息记录，通过 cache.stats() 获取
+                .build(new CacheLoader<String, String>() { // 获得 LocalLoading
+                    //
                     @Override
                     public String load(String key) throws Exception {
-                        log.info("<----------- load key: {}", key);
+                        log.debug("<----------- load key: {}", key);
                         return "cache-" + key;
                     }
 
                     //
                     @Override
                     public Map<String, String> loadAll(Iterable<? extends String> keys) throws Exception {
-                        log.info("<----------- load all keys: {}", keys);
+                        log.debug("<----------- load all keys: {}", keys);
                         return super.loadAll(keys);
                     }
                 });
 
-        List<String> keys = Lists.newArrayList();
+        log.info("================== put manually");
+        cache.put("key-put-1", "test"); // 手动放入缓存
+        cache.putAll(new HashMap<>());
+
+        log.info("================== remove manually");
+        cache.invalidate("key-put-1");
+        cache.invalidateAll();
+
+        log.info("================== loading if miss");
+        List<String> keys = Lists.newArrayList(); // 准备 load key
         for (int i = 0; i < 100; i++) {
             String key = "key-" + i;
             keys.add(key);
         }
 
-        log.info("================== load one");
+        /*
+         * LoadingCache.get（k） ->  CacheLoader.load(k)
+         * LoadingCache.refresh(k) ->   CacheLoader.reload(k)
+         * LoadingCache.getAll(keys) -> CacheLoader.loadAll(keys)
+         */
+        log.info("================== - load one");
         keys.forEach(key -> {
-            Assertions.assertNull(cache.getIfPresent(key)); // null cause non cache
+            // null cause non cache
+            Assertions.assertNull(cache.getIfPresent(key)); // 💡第一次获取，没有数据
+            // load and cache and do not throw checked exception
             // cache.get(key); // load
-            String one = cache.getUnchecked(key);// load and cache and do not throw checked exception
+            String one = cache.getUnchecked(key); // 💡获取次数超过缓存最大数量后，触发 remove listen 回调
+            // load from cache
             // cache.get(key); // cache
-            cache.getUnchecked(key); // load from cache
+            cache.getUnchecked(key); // 💡已缓存：再次获取，没有调用 load 方法
         });
 
-        log.info("================== load all");
+        log.info("================== - load all");
         try {
-            ImmutableMap<String, String> all = cache.getAll(keys);
+            ImmutableMap<String, String> all = cache.getAll(keys); // 💡调用 loadAll 方法
         } catch (ExecutionException e) {
             throw new RuntimeException(e);
         }
+
+        log.info("================== stats");
+        log.debug("stats: {}", cache.stats());
     }
 
+    /**
+     * 问题处理：返回 null 时，抛出异常的处理
+     */
+    @DisplayName("CacheLoader 测试：返回 null 问题")
     @Test
     void testCacheLoad_null() {
         {
@@ -102,5 +157,10 @@ public class CacheLoaderTest {
             Optional<String> one = cache.getUnchecked("key-null");
             Assertions.assertFalse(one.isPresent());
         }
+    }
+
+    @Test
+    void testSpec() {
+        // todo 缓存参数文件配置
     }
 }
--- src/zh/dev-java-commonsense/sdk-toolkit-guava.md
+++ src/zh/dev-java-commonsense/sdk-toolkit-guava.md
@@ -71,20 +71,38 @@ alternate:
 
 ## Cache
 
-In Menory 缓存
+In Menory cache 缓存
 
-alternate
+> alternate
+>
+> - Apache commons JUC
+> - OsCache
+> - SwarmCache
+> - Ehcache
 
-- Apache commons JUC
-- OsCache
-- SwarmCache
-- Ehcache
+基本接口
+
+| 接口/类                  | 描述                                                                |
+| ------------------------ | ------------------------------------------------------------------- |
+| `Cache<K, V>`            | 缓存的核心接口。表示一种能够存储键值对的缓存结构                    |
+| `LoadingCache<K, V>`     | 【继承 Cache 接口】 用于在缓存中自动加载缓存项                      |
+| `LocalManualCache<K, V>` | 【继承 Cache 接口】 每次取数据时，指定缓存加载方式 （类似 ehcache） |
+| `CacheLoader<K, V>`      | 在使用 LoadingCache 时提供加载缓存项的逻辑                          |
+| `CacheBuilder`           | 用于创建 Cache 和 LoadingCache 实例的构建器类                       |
+| `CacheStats`             | 用于表示缓存的统计信息，如命中次数、命中率、加载次数、存储次数等    |
+| `RemovalListener<K, V>`  | 用于监听缓存条目被移除的事件，并在条目被移除时执行相应的操作        |
+
+具体接口说明： <https://blog.csdn.net/JokerLJG/article/details/134596900>
 
 ### 原理：LRU 算法
 
 LRU（Least Recently Used，最少最近使用）
 
 todo https://www.bilibili.com/video/BV1R4411s7GX?p=29
+
+todo LinkedHashMap 实现
+
+todo LinkedList 实现
 
 ### 原理：引用类型
 
@@ -93,8 +111,33 @@ todo https://www.bilibili.com/video/BV1R4411s7GX?p=29
 - WeakReference 弱引用 —— 尽管还有引用，但是会被 Major gc （仅清理老年代） 和 full gc （清理整个堆） 回收
 - PhantomReference 幽灵引用 —— 尽管还有引用，但不管有没有被 gc 回收，都是无法通过引用访问内存内容，**但是可以收到该内存被 gc 回收的通知** | 参考： apache common-io FileCleaningTracker
 
-### 例子
+todo 内存敏感实现
+
+### 例子：get-if-absent-compute
+
+Guava Cache 提供两种实现了 get-if-absent-compute 语义的方式：
+
+> 所谓 get-if-absent-compute 语义：在调用 get 方法时，如果发现指定的值不存在，则通过加载、计算等方式来提供值。也可理解为 lazy load（懒加载、按需加载）。
+
+- `Cache.get(key, Callable)` —— 在调用 get 时，指定一个 Callable，如果值不存在时，调用 Callable 来计算值。计算到值后放入 Cache 中，并返回结果。
+- `LoadingCache` —— 定义 Cache 时提供一个 CacheLoader 指定统一的缓存加载方式。
+
+  LoadingCache 与 CacheLoader 的几个方法的调用关系： （CacheLoader 是不保证一定可以加载成功，所以它的所有方法都是有异常的）
+
+  ```
+  LoadingCache.get（k） ->  CacheLoader.load(k)
+  LoadingCache.refresh(k) ->   CacheLoader.reload(k)
+  LoadingCache.getAll(keys) -> CacheLoader.loadAll(keys)
+  ```
 
 ```java
 <!-- @include: @project/code/demo-guava/demo-01-simple/src/test/java/org/example/guava/CacheLoaderTest.java -->
 ```
+
+### 策略：清理、过期
+
+Guava Cache 采用基于容量、Soft 引用、Weak 引用的清理触发条件，基于过期时间、权重来决定哪些数据优先清理，采用 LRU 算法来清理数据。
+
+### 策略：重载 refresh
+
+LoadingCache 中的 refresh 提供了值替换的功能。在调用 refresh 时，会先调加载新值，新值加载到后替换掉老值，并返回老值。如果加载不到新值，老值是被保留的，不会被替换掉的。
