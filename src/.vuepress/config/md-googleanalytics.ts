43930963e15d95910563ddad70bb4612f89f91e9
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2024-10-11 07:14:53 +0800

    init: import google-analytics (fix version)

--- src/.vuepress/config/md-googleanalytics.ts
+++ src/.vuepress/config/md-googleanalytics.ts
@@ -3,6 +3,6 @@ import { googleAnalyticsPlugin } from "@vuepress/plugin-google-analytics";
 export default {
   googleAnalyticsPlugin: googleAnalyticsPlugin({
     // we have multiple deployments, which would use different id
-    id: process.env.DOCS_GA_ID ?? "G-XXXXXXXXXX",
+    id: process.env.DOCS_GA_ID ?? "",
   }),
 };
