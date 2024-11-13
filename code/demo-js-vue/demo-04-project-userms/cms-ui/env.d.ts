e6f32b79e2ad8897eff595c9b2bc8eac33208115
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2024-11-14 07:34:43 +0800

    js(vue/project01): axios 封装 （多环境）

--- code/demo-js-vue/demo-04-project-userms/cms-ui/env.d.ts
+++ code/demo-js-vue/demo-04-project-userms/cms-ui/env.d.ts
@@ -6,3 +6,13 @@
 //   const component: DefineComponent;
 //   export default component;
 // }
+
+interface ImportMetaEnv {
+  readonly VITE_APP_BASE_TARGET: string;
+  readonly VITE_APP_BASE_API: string;
+  // 更多环境变量...
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
--- code/demo-js-vue/demo-04-project-userms/cms-ui/src/service/config/index.ts
+++ code/demo-js-vue/demo-04-project-userms/cms-ui/src/service/config/index.ts
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:8099";
+const BASE_TARGET = import.meta.env.VITE_APP_BASE_TARGET;
+const BASE_API = import.meta.env.VITE_APP_BASE_API;
+export const BASE_URL = BASE_TARGET + BASE_API;
 export const TIME_OUT = 10000;
