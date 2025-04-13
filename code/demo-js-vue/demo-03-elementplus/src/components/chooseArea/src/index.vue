ee415a8c97be9817e1b9365a9c396f25ed4f94a7
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2025-04-13 17:12:44 +0800

    [#][docs] elementplus el-select

--- code/demo-js-vue/demo-03-elementplus/src/components/chooseArea/src/index.vue
+++ code/demo-js-vue/demo-03-elementplus/src/components/chooseArea/src/index.vue
@@ -1,40 +1,84 @@
 <template>
-  <div style="display: flex;">
-    <el-select placeholder="请选择省份" v-model="selectedProvince">
-      <el-option v-for="item in provinces" :key="item.code" :value="item.code" :label="item.name">{{ item.name }}</el-option>
+  <div style="display: flex">
+    <el-select clearable placeholder="请选择省份" v-model="selectedProvince">
+      <el-option
+        v-for="item in provinces"
+        :key="item.code"
+        :value="item.code"
+        :label="item.name"
+        >{{ item.name }}</el-option
+      >
     </el-select>
-    <el-select :disabled="!selectedProvince" style="margin: 0 10px;" placeholder="请选择城市" v-model="selectedCity">
-      <el-option v-for="item in cities" :key="item.code" :value="item.code" :label="item.name">{{ item.name }}</el-option>
+    <el-select
+      clearable
+      :disabled="!selectedProvince"
+      style="margin: 0 10px"
+      placeholder="请选择城市"
+      v-model="selectedCity"
+    >
+      <el-option
+        v-for="item in cities"
+        :key="item.code"
+        :value="item.code"
+        :label="item.name"
+        >{{ item.name }}</el-option
+      >
     </el-select>
-    <el-select :disabled="!selectedCity" placeholder="请选择区域" v-model="selectedArea">
-      <el-option v-for="item in areas" :key="item.code" :value="item.code" :label="item.name">{{ item.name }}</el-option>
+    <el-select
+      clearable
+      :disabled="!selectedCity"
+      placeholder="请选择区域"
+      v-model="selectedArea"
+    >
+      <el-option
+        v-for="item in areas"
+        :key="item.code"
+        :value="item.code"
+        :label="item.name"
+        >{{ item.name }}</el-option
+      >
     </el-select>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue';
-import allAreas from './lib/pca-code.json'
+import { ref, computed, watch } from "vue";
+import allAreas from "./lib/pca-code.json";
 
-const selectedProvince = ref('')
-const selectedCity = ref('')
-const selectedArea = ref('') 
+const selectedProvince = ref("");
+const selectedCity = ref("");
+const selectedArea = ref("");
 
-const provinces = ref(allAreas)
-const cities = computed(() => {
-  if (!selectedProvince.value) {
-    return [];
-  } else {
-    return provinces.value.find(item => item.code === selectedProvince.value)!.children;
+const provinces = ref<any>(allAreas);
+const cities = ref<any>([]);
+const areas = ref<any>([]);
+
+watch(
+  () => selectedProvince.value,
+  (val) => {
+    if (!val) {
+      cities.value = [];
+    } else {
+      cities.value = provinces.value.find(
+        (item: { code: string }) => item.code === val
+      )!.children;
+    }
+    selectedCity.value = "";
   }
-})
-const areas = computed(() => {
-  if (!selectedCity.value) {
-    return []
-  } else {
-    return cities.value.find(item => item.code === selectedCity.value)?.children
+);
+watch(
+  () => selectedCity.value,
+  (val) => {
+    if (!val) {
+      areas.value = [];
+    } else {
+      areas.value = cities.value.find(
+        (item: { code: string }) => item.code === val
+      )!.children;
+    }
+    selectedArea.value = "";
   }
-})
+);
 </script>
 
 <style scoped></style>
