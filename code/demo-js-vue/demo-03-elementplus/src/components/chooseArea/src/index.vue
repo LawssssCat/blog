3621e9be9a39c2e4d33a996ef06eb20daf73bdde
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2025-04-13 17:12:45 +0800

    [#][docs] elementplus el-select

--- code/demo-js-vue/demo-03-elementplus/src/components/chooseArea/src/index.vue
+++ code/demo-js-vue/demo-03-elementplus/src/components/chooseArea/src/index.vue
@@ -42,16 +42,30 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed, watch } from "vue";
+import { ref, unref, computed, watch } from "vue";
 import allAreas from "./lib/pca-code.json";
+
+export interface AreaItem {
+  name: string;
+  code: string;
+  children?: AreaItem[];
+}
+
+export interface Data {
+  name: string;
+  code: string;
+}
+
+// 分发事件给父组件
+const emits = defineEmits(["change"]);
 
 const selectedProvince = ref("");
 const selectedCity = ref("");
 const selectedArea = ref("");
 
-const provinces = ref<any>(allAreas);
-const cities = ref<any>([]);
-const areas = ref<any>([]);
+const provinces = ref<AreaItem[]>(allAreas);
+const cities = ref<AreaItem[]>([]);
+const areas = ref<AreaItem[]>([]);
 
 watch(
   () => selectedProvince.value,
@@ -60,8 +74,8 @@ watch(
       cities.value = [];
     } else {
       cities.value = provinces.value.find(
-        (item: { code: string }) => item.code === val
-      )!.children;
+        (item) => item.code === val
+      )!.children!;
     }
     selectedCity.value = "";
   }
@@ -72,11 +86,33 @@ watch(
     if (!val) {
       areas.value = [];
     } else {
-      areas.value = cities.value.find(
-        (item: { code: string }) => item.code === val
-      )!.children;
+      areas.value = cities.value.find((item) => item.code === val)!.children!;
     }
     selectedArea.value = "";
+  }
+);
+watch(
+  () => selectedArea.value,
+  () => {
+    // console.log(selectedProvince.value, selectedCity.value, selectedArea.value);
+    const fn = (arr: AreaItem[], code: any): Data => {
+      const { code: c, name: n } = unref(arr).find(
+        (item) => item.code === code
+      )!;
+      return {
+        code: c,
+        name: n,
+      };
+    };
+    let province: Data = fn(provinces.value, selectedProvince.value);
+    let city: Data = fn(cities.value, selectedCity.value);
+    let area: Data = fn(areas.value, selectedArea.value);
+    // console.log(province, city, area);
+    emits("change", {
+      province,
+      city,
+      area,
+    });
   }
 );
 </script>
--- code/demo-js-vue/demo-03-elementplus/src/views/chooseArea/index.vue
+++ code/demo-js-vue/demo-03-elementplus/src/views/chooseArea/index.vue
@@ -1,13 +1,23 @@
 <template>
   <div>
-    <m-choose-area></m-choose-area>
+    <m-choose-area @change="handleAreaChange"></m-choose-area>
   </div>
 </template>
 
 <script setup lang="ts">
-import mChooseArea from '@/components/chooseArea/src/index.vue'
+import mChooseArea, { type Data } from "@/components/chooseArea/src/index.vue";
+
+function handleAreaChange({
+  province,
+  city,
+  area,
+}: {
+  province: Data;
+  city: Data;
+  area: Data;
+}) {
+  console.log(province, city, area);
+}
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
