88310e5a54acec7e9a7c6b271899283c6af60a67
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2025-10-05 13:58:08 +0800

    [#][docs] c/c++ development env demo
    + update vagrant script

--- code/demo-c-base/README.md
+++ code/demo-c-base/README.md
@@ -1,45 +1,48 @@
-# C/C++基础测试
+# C/C++开发环境集合
 
-用vagrant起虚拟机，在Centos中搭建开发环境
+用vagrant管理虚拟机Fedora系统。
+方便在Linux中搭建C/C++的开发环境。
 
-1、启动虚拟机
+目录：
+
++ 各级别开发环境
+  + [vs:gcc,gdb](demo-01-workspace/01-gcc-gdb)
+  + [vs:gcc,cmake,gdb](demo-01-workspace/02-gcc-cmake-gdb)
+  + [vs:gcc,cmake,gtest,gdb](demo-01-workspace/03-gcc-cmake-gtest-gdb)
+  + [vs:vcpkg,gcc,cmake,gtest,gdb](demo-01-workspace/04-vcpkg-gcc-cmake-gtest-gdb)
+  + [vs:vcpkg,clang-llvm,cmake,gtest,lldb](demo-01-workspace/01-vcpkg-clang_llvm-cmake-gtest-gdb) —— best
+
+## 常用Vagrant操作
+
+1、虚拟机管理
 
 ```bash
-vagrant up
+# 启动虚拟机
+$ vagrant up
+
+# 重载虚拟机设置
+$ vagrant reload             # 重载： Vagrantfile
+$ vagrant provision          # 重载： vagrant.sh
+$ vagrant reload --provision # 重载： Vagrantfile + vagrant.sh
+
+# 销毁虚拟机
+$ vagrant destroy
 ```
 
-2、配置登录信息
+2、会话登录
 
 ```bash
-$ su -
-vagrant
+# 方式1： 使用vagrant功能
+# 问题： 没法方便的在xshell、vscode等会话管理软件上
+$ vagrant ssh
 
-# 开放会话管理功能
-$ vi /etc/ssh/sshd_config
-PasswordAuthentication yes
-AllowAgentForwarding yes
-AllowTcpForwarding yes
-$ systemctl restart sshd.service
-
-# 生成登录秘钥
-$ ssh-keygen
-$ ssh-copy-id localhost
-vagrant
-$ 考出来，配进去 ~/.ssh/config
-Host vagrant-cppdev
-  HostName 192.168.56.20
-  User vagrant
-  IdentityFile .ssh/id_ed25519
-```
-
-编译环境（参考：<https://www.cnblogs.com/lenmom/p/9193388.html>）
-
-```bash
-# 镜像源
-sudo sed -e 's|^metalink=|#metalink=|g' \
-    -e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.tuna.tsinghua.edu.cn/fedora|g' \
-    -i.bak \
-    /etc/yum.repos.d/fedora.repo \
-    /etc/yum.repos.d/fedora-updates.repo
-sudo yum makecache
+# 方式2： 使用公开密钥对 （❗勿在公网使用）
+$ # cat .ssh/id_ed25519
+$ # cat .ssh/id_ed25519.pub
+# e.g.
+# $ vim ~/.ssh/config
+# Host vagrant-cppdev
+#   HostName 192.168.56.20
+#   User vagrant
+#   IdentityFile .ssh/id_ed25519
 ```
--- code/demo-c-base/vagrant.sh
+++ code/demo-c-base/vagrant.sh
@@ -1,8 +1,97 @@
 #!/bin/bash
 
-set -ex
+set -e
 
-mkdir ~/.bashrc.d
-cat > ~/.bashrc.d <<EOF
-umask 007
+VM_PROVIDE="$1"
+VM_NAME="$2"
+VM_IPADDR="$3"
+
+SCRIPT_PATH="$(cd $(dirname $0); pwd)"
+VGHOME_PATH="/vagrant"
+VGUSER_HOME="/home/vagrant"
+
+#####################################
+echo "--- [print system] ---"
+uname -a
+arch
+cat /etc/os-release
+whoami
+
+
+#####################################
+echo "--- [write script] ---"
+su - vagrant
+cat > $VGUSER_HOME/bootstrap-buildchain.sh <<EOF
+#!/bin/bash
+set -ex
+echo ==================== common bashrc ====================
+VGUSER_BASHRC_HOME="$VGUSER_HOME/.bashrc.d"
+mkdir -pv "\$VGUSER_BASHRC_HOME" && echo umask 007 > \$VGUSER_BASHRC_HOME/common.sh
+echo ==================== config sshd ====================
+# sudo sed -i 's/^[# ]*PasswordAuthentication .*$/PasswordAuthentication yes/' /etc/ssh/sshd_config
+# sudo sed -i 's/^[# ]*AllowAgentForwarding .*$/AllowAgentForwarding yes/' /etc/ssh/sshd_config
+# sudo sed -i 's/^[# ]*AllowTcpForwarding .*$/AllowTcpForwarding yes/' /etc/ssh/sshd_config
+sudo sed -i 's/^[# ]*X11Forwarding .*$/X11Forwarding yes/' /etc/ssh/sshd_config
+sudo systemctl restart sshd.service
+echo ==================== config ssh ====================
+SSH_PUB="$(cat $VGHOME_PATH/.ssh/id_ed25519.pub)"
+sed "/\$SSH_PUB/d" $VGUSER_HOME/.ssh/authorized_keys
+echo \$SSH_PUB >> $VGUSER_HOME/.ssh/authorized_keys
+cat $VGUSER_HOME/.ssh/authorized_keys
+echo ================== 镜像源 ==================
+sudo sed -e 's|^metalink=|#metalink=|g' \
+  -e 's|^#baseurl=http://download.example/pub/fedora/linux|baseurl=https://mirrors.jlu.edu.cn/fedora|g' \
+  -i.bak \
+  /etc/yum.repos.d/fedora.repo \
+  /etc/yum.repos.d/fedora-updates.repo
+# sudo yum clean dbcache
+sudo yum makecache
+echo ==================
+echo 基础开发调试环境环境
+echo ==================
+sudo yum -y install gcc gcc-g++ make cmake gdb
+gcc --version   # 编译工具
+g++ --version   # 编译工具
+make --version  # 项目组织工具
+cmake --version # 项目组织工具
+gdb --version   # 调试工具
+echo ##########################
+echo # 项目集成开发环境
+echo ##########################
+sudo yum -y install clang clangd lldb ninja
+clang --version  # 编译工具
+ninja --version  # 项目组织工具
+clangd --version # LSP
+lldb --version   # 调试工具
+sudo yum -y install git zip unzip
+git --version
+zip --version
+function install_vcpkg {
+  local install_home="\$1"
+  if [ ! -f "\$install_home/vcpkg" ]; then
+    rm -rf "\$install_home"
+    mkdir -pv "\$install_home"
+    cd "\$install_home"
+    git clone -v --progress --depth 1 --branch 2025.09.17 https://github.com/microsoft/vcpkg.git .
+    ./bootstrap-vcpkg.sh
+  fi
+  cat > \$VGUSER_BASHRC_HOME/vcpkg.sh <<EOF2
+export PATH=\$install_home:\$PATH
+export VCPKG_ROOT=\$install_home
+EOF2
+  source ~/.bashrc
+  vcpkg --version
+}
+install_vcpkg "$VGUSER_HOME/.local/vcpkg"
 EOF
+
+
+#####################################
+echo "--- [introduce] ---"
+echo
+echo run:
+echo
+echo vagrant ssh -c \"bash $VGUSER_HOME/bootstrap-buildchain.sh\"
+echo
+echo to install build chain.
+echo
