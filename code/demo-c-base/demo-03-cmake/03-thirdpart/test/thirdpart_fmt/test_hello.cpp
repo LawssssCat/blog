e8cc4a543eeca90c78afbc811498d1054f0b06d1
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2025-11-06 23:55:47 +0800

    [#][docs] c/c++ cmake - thirdpart: fmt

--- code/demo-c-base/demo-03-cmake/03-thirdpart/test/thirdpart_fmt/test_hello.cpp
+++ code/demo-c-base/demo-03-cmake/03-thirdpart/test/thirdpart_fmt/test_hello.cpp
@@ -1,9 +1,14 @@
 #include "fmt/base.h"
+#include "fmt/format.h"
 #include <iostream>
 
 #include <gtest/gtest.h>
 
 #include <fmt/core.h>
+#include <locale>
+
+// 参考：
+// https://awesometop.cn/posts/0b20b1ca03b24e60ac6bbb9f6400c79f
 
 TEST(test_fmt, show_world) {
   std::cout << "hello world ~ " << FMT_VERSION << std::endl;
@@ -20,7 +25,13 @@ TEST(test_fmt, show_world) {
   // {:f} —— 浮点数
   // {:d} —— 十进制整数
   // {:x} —— 十六进制
-  fmt::println("name:{1}, age:{0:.5}", "123", "knox");
+  fmt::println("name:{1}, age:{0:.5}", "123456", "knox");
+  fmt::println("price:{0:.5f}", 22.f);
+
+  auto s_no = fmt::format("{:L}", 1234567890);
+  auto s_en = fmt::format(std::locale("en_US.UTF-8"), "{:L}", 1234567890);
+  fmt::println("normal:{}", s_no); // 1234567890
+  fmt::println("locale:{}", s_en); // 1,234,567,890
 
   // class/struct toString
   // todo
