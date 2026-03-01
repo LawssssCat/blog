33254a179f2ac72fbfe2c0ce0b9fc2a8a15f2748
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2026-03-02 02:35:56 +0800

    [#] 爬虫问题
    see https://github.com/ai-robots-txt/ai.robots.txt?tab=readme-ov-file

--- src/.vuepress/config/footer.ts
+++ src/.vuepress/config/footer.ts
@@ -5,7 +5,11 @@ export default {
   displayFooter: true,
   locales: {
     "/zh/": {
-      copyright: "Copyright © 2025-2026 Steven",
+      copyright:
+        "Copyright © 2025-2026 Steven" +
+        "<br>license: <a href='https://creativecommons.org/licenses/by/4.0/'>CC 4.0</a>" +
+        "<br>engine: <a href='https://vuepress.vuejs.org/'>vuepress</a>" +
+        "<br>theme: <a href='https://theme-hope.vuejs.press/'>vuepress-hope</a>",
     },
   },
 };
