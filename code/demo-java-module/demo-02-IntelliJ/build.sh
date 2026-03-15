9e5619ad0dd8ed4e4e0f789fc359d68b82b55d01
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2026-03-15 11:04:53 +0800

    [#] [docs] java
    + java module idea 结构

---
+++ code/demo-java-module/demo-02-IntelliJ/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# 说明：需要手动指定所有模块位置，否则报错
+# e.g.
+# $ java --module-path "./out/production/demo-02-IntelliJ" --module "demo.main/org.example.Main"
+# Error occurred during initialization of boot layer
+# java.lang.module.FindException: Module demo.lib not found, required by demo.main
+java --module-path "./out/production/demo-02-IntelliJ;./out/production/MyLib" --module "demo.main/org.example.Main"
+
+java -jar "./out/artifacts/demo_02_IntelliJ_jar/demo-02-IntelliJ.jar"
