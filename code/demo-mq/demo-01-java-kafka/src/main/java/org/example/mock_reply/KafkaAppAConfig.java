47207e244653e067b805b3b4adc81b62a1c9e3e6
lawsssscat <18041500+LawssssCat@users.noreply.github.com>
2026-06-20 16:32:23 +0800

    [#] [docs] kafka
    + 同步接口、异步业务 （专用：ReplyingKafkaTemplate）

--- code/demo-mq/demo-01-java-kafka/src/main/java/org/example/mock_reply/KafkaAppAConfig.java
+++ code/demo-mq/demo-01-java-kafka/src/main/java/org/example/mock_reply/KafkaAppAConfig.java
@@ -1,5 +1,6 @@
 package org.example.mock_reply;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -14,51 +15,56 @@ import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import java.time.Duration;
 import java.util.UUID;
 
-@Configuration
+
+/**
+ * 不将ReplyingKafkaTemplate注册为全局bean
+ */
+
+//@Configuration
 public class KafkaAppAConfig {
-    // 1. 配置用來監聽 B 回覆 (Topic_Response) 的監聽容器
-    @Bean
-    public ConcurrentMessageListenerContainer<String, String> replyContainer(ConsumerFactory<String, String> cf) {
-        ContainerProperties containerProperties = new ContainerProperties(
-                "Topic_2_Response"
-        );
-        // 💥 叢集環境關鍵：每個 A 實例必須有唯一的 Group ID（例如加上動態 UUID）
-        // 這樣 B 回傳的訊息才會廣播給所有 A 實例，確保真正發起請求的那台機器能收到回覆
-        containerProperties.setGroupId("group_reply_a_" + UUID.randomUUID().toString().substring(0, 8));
-        return new ConcurrentMessageListenerContainer<>(cf, containerProperties);
-    }
-
-    // 2. 定義 ReplyingKafkaTemplate
-    @Bean
-    public ReplyingKafkaTemplate<String, String, String> replyingKafkaTemplate(ProducerFactory<String, String> pf,
-            ConcurrentMessageListenerContainer<String, String> replyContainer) {
-
-        ReplyingKafkaTemplate<String, String, String> replyTemplate = new ReplyingKafkaTemplate<>(pf, replyContainer);
-
-        // 可选配置：強制將 Correlation ID 在發送時轉為純文字 String 塞入 Header
-        /**
-         * 配置前：
-         * <code>
-         *     CreateTime:1781940518839        kafka_correlationId:�� �]@t�"�;���       null    B_處理完成_[{"action": "create_order", "userId": 123}]
-         * </code>
-         * 配置后：
-         * <code>
-         *     CreateTime:1781942527880        kafka_correlationId:40ede50e-a0d4-4e1e-a381-8cb6e181fd82        null    B_處理完成_[{"action": "create_order", "userId": 123}]
-         * </code>
-         */
-//        replyTemplate.setCorrelationIdStrategy(record -> {
-//            return new CorrelationKey()
-//        });
-        replyTemplate.setBinaryCorrelation(false);
-
-        // 設定全域預設超時時間（例如 5 秒）
-        replyTemplate.setDefaultReplyTimeout(Duration.ofSeconds(5));
-        return replyTemplate;
-    }
-
-    @Bean
-    @Primary // 💥 關鍵：標記為主要 Bean，當 @Autowired 沒有指定時，預設用這個
-    public KafkaTemplate kafkaTemplate(ProducerFactory pf) {
-        return new KafkaTemplate<>(pf);
-    }
+//    // 1. 配置用來監聽 B 回覆 (Topic_Response) 的監聽容器
+//    @Bean("orderReplyContainer")
+//    public ConcurrentMessageListenerContainer<String, String> replyContainer(ConsumerFactory<String, String> cf) {
+//        ContainerProperties containerProperties = new ContainerProperties(
+//                "Topic_2_Response"
+//        );
+//        // 💥 叢集環境關鍵：每個 A 實例必須有唯一的 Group ID（例如加上動態 UUID）
+//        // 這樣 B 回傳的訊息才會廣播給所有 A 實例，確保真正發起請求的那台機器能收到回覆
+//        containerProperties.setGroupId("group_reply_a_" + UUID.randomUUID().toString().substring(0, 8));
+//        return new ConcurrentMessageListenerContainer<>(cf, containerProperties);
+//    }
+//
+//    // 2. 定義 ReplyingKafkaTemplate
+//    @Bean("orderReplyingKafkaTemplate")
+//    public ReplyingKafkaTemplate<String, String, String> replyingKafkaTemplate(ProducerFactory<String, String> pf,
+//            @Qualifier("orderReplyContainer") ConcurrentMessageListenerContainer<String, String> replyContainer) {
+//
+//        ReplyingKafkaTemplate<String, String, String> replyTemplate = new ReplyingKafkaTemplate<>(pf, replyContainer);
+//
+//        // 可选配置：強制將 Correlation ID 在發送時轉為純文字 String 塞入 Header
+//        /**
+//         * 配置前：
+//         * <code>
+//         *     CreateTime:1781940518839        kafka_correlationId:�� �]@t�"�;���       null    B_處理完成_[{"action": "create_order", "userId": 123}]
+//         * </code>
+//         * 配置后：
+//         * <code>
+//         *     CreateTime:1781942527880        kafka_correlationId:40ede50e-a0d4-4e1e-a381-8cb6e181fd82        null    B_處理完成_[{"action": "create_order", "userId": 123}]
+//         * </code>
+//         */
+////        replyTemplate.setCorrelationIdStrategy(record -> {
+////            return new CorrelationKey()
+////        });
+//        replyTemplate.setBinaryCorrelation(false);
+//
+//        // 設定全域預設超時時間（例如 5 秒）
+//        replyTemplate.setDefaultReplyTimeout(Duration.ofSeconds(5));
+//        return replyTemplate;
+//    }
+//
+//    @Bean
+//    @Primary // 💥 關鍵：標記為主要 Bean，當 @Autowired 沒有指定時，預設用這個
+//    public KafkaTemplate kafkaTemplate(ProducerFactory pf) {
+//        return new KafkaTemplate<>(pf);
+//    }
 }
--- code/demo-mq/demo-01-java-kafka/src/main/java/org/example/mock_reply/a/OrderV2Controller.java
+++ code/demo-mq/demo-01-java-kafka/src/main/java/org/example/mock_reply/a/OrderV2Controller.java
@@ -1,10 +1,15 @@
 package org.example.mock_reply.a;
 
+import jakarta.annotation.PreDestroy;
 import jakarta.annotation.Resource;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.listener.ConcurrentMessageListenerContainer;
+import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.requestreply.ReplyingKafkaTemplate;
 import org.springframework.kafka.requestreply.RequestReplyFuture;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,14 +17,45 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Duration;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 @RestController
 @RequestMapping("/order/v2")
 public class OrderV2Controller {
-    @Resource
+    // 宣告為成員變數
+//    @Resource(name = "orderReplyingKafkaTemplate")
     private ReplyingKafkaTemplate<String, String, String> replyingKafkaTemplate;
+
+    // 透過構造函數注入基礎的 Factory
+    public OrderV2Controller(ProducerFactory<String, String> pf, ConsumerFactory<String, String> cf) {
+
+        // 1. 建立監聽容器
+        ContainerProperties containerProperties = new ContainerProperties("Topic_2_Response");
+        containerProperties.setGroupId("group_order_reply_" + UUID.randomUUID().toString().substring(0, 8));
+        ConcurrentMessageListenerContainer<String, String> container =
+                new ConcurrentMessageListenerContainer<>(cf, containerProperties);
+
+        // 2. new 出 Template
+        this.replyingKafkaTemplate = new ReplyingKafkaTemplate<>(pf, container);
+        this.replyingKafkaTemplate.setBinaryCorrelation(false);
+        this.replyingKafkaTemplate.setDefaultReplyTimeout(Duration.ofSeconds(5));
+
+        // 💥 核心關鍵：因為不是 Bean，Spring 啟動時不會理它。
+        // 你必須手動呼叫 start()，它才會開始監聽 Kafka 的回覆 Topic！
+        this.replyingKafkaTemplate.start();
+    }
+
+
+    // 💡 良好的習慣：在 Service 銷毀時手動關閉，釋放 Kafka 連線資源
+    @PreDestroy
+    public void destroy() {
+        if (this.replyingKafkaTemplate != null) {
+            this.replyingKafkaTemplate.stop();
+        }
+    }
 
     @PostMapping("/process")
     public ResponseEntity<String> callBViaKafka(@RequestBody String requestData) {
